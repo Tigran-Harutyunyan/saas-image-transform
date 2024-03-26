dfefe58bfa42ff59ceb709501150a34ecf8885e4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-26 17:00:22 +0400

    added Clerk

---
+++ pages/sign-in.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SignIn } from "vue-clerk";
+useHead({
+  title: "AI images | Sign-in",
+});
+definePageMeta({
+  layout: "auth",
+  middleware: ["public"],
+});
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <SignIn sign-up-url="/sign-up" />
+  </div>
+</template>
---
+++ pages/sign-up.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SignUp } from "vue-clerk";
+useHead({
+  title: "AI images | Sign-in",
+});
+definePageMeta({
+  layout: "auth",
+  middleware: ["public"],
+});
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <SignUp sign-in-url="/sign-in" />
+  </div>
+</template>
