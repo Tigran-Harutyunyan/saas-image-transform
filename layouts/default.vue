9a0bb4de209920b6fe9c3c95cad379507d063c12
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-26 17:01:09 +0400

    created incomplete index page a layouts

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+  <main class="flex min-h-screen w-full flex-col bg-white lg:flex-row">
+    <Sidebar />
+    <MobileNav />
+
+    <div
+      class="mt-16 flex-1 overflow-auto py-8 lg:mt-0 lg:max-h-screen lg:py-10"
+    >
+      <div
+        class="max-w-5xl mx-auto px-5 md:px-10 w-full text-dark-400 p-16-regular"
+      >
+        <slot />
+      </div>
+    </div>
+
+    <Toaster />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { navLinks } from "@/constants";
+//import { getAllImages } from "@/lib/actions/image.actions";
+
+definePageMeta({ middleware: "public" });
+
+useHead({
+  title: "Web site builder | Home",
+});
+const { query } = useRoute();
+
+const page = Number(query?.page) || 1;
+const searchQuery = (query?.query as string) || "";
+
+const images = []; // await getAllImages({ page, searchQuery });
+</script>
+<template>
+  <section class="home">
+    <h1 class="home-heading">Unleash Your Creative Vision with Imaginify</h1>
+    <ul class="flex-center w-full gap-20">
+      <NuxtLink
+        v-for="link in navLinks.slice(1, 5)"
+        :key="link.route"
+        :to="link.route"
+        class="flex-center flex-col gap-2"
+      >
+        <li class="flex-center w-fit rounded-full bg-white p-4">
+          <NuxtImg :src="link.icon" alt="image" :width="24" :height="24" />
+        </li>
+        <p class="p-14-medium text-center text-white">{{ link.label }}</p>
+      </NuxtLink>
+    </ul>
+  </section>
+
+  <section class="sm:mt-12">
+    <!-- <Collection
+      :hasSearch="true"
+      :images="images?.data"
+      :totalPages="images?.totalPage"
+      :page="page"
+    /> -->
+  </section>
+</template>
