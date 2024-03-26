61361ff8326bdd33927a1fcc7697be7df58a9187
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-26 17:07:41 +0400

    added dummy pages

---
+++ pages/[...404].vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Button } from "~/components/ui/button";
+definePageMeta({ layout: "auth" });
+useHead({
+  title: "SaaS | 404",
+});
+</script>
+
+<template>
+  <div
+    class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16"
+  >
+    <div class="w-full lmd:w-1/2 relative pb-12 lg:pb-0">
+      <div class="relative flex items-center flex-col">
+        <div class="mb-8 mx-auto">
+          <img src="https://i.ibb.co/G9DC8S0/404-2.png" class="mx-auto" />
+        </div>
+        <NuxtLink to="/" class="mx-auto">
+          <Button variant="primary" class="text-white text-lg"
+            >BACK TO HOMEPAGE</Button
+          >
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/transformations/add/[type]/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { transformationTypes } from "@/constants";
+
+const route = useRoute();
+
+console.log(route.params?.type);
+
+const type = route.params?.type as string;
+
+if (!type) navigateTo("/");
+
+const transformation = transformationTypes[type];
+</script>
+
+<template>
+  <Header :title="transformation.title" :subtitle="transformation.subTitle" />
+
+  <section class="mt-10">
+    <!-- <TransformationForm
+      action="Add"
+      :userId="user._id"
+      :type="transformation.type as TransformationTypeKey"
+      :creditBalance="user.creditBalance"
+    /> -->
+  </section>
+</template>
