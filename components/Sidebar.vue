16fe05bd4f274b4b51fa399cb2aa10a962081542
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-26 17:03:01 +0400

    created Sidebar and SidebarItem components

---
+++ components/Sidebar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { UserButton, SignedIn, SignedOut } from "vue-clerk";
+
+import { navLinks } from "@/constants";
+</script>
+
+<template>
+  <aside class="sidebar">
+    <div class="flex size-full flex-col gap-4">
+      <NuxtLink too="/" class="sidebar-logo">
+        <NuxtImg
+          src="/assets/images/logo-text.svg"
+          alt="logo"
+          :width="180"
+          :height="28"
+        />
+      </NuxtLink>
+
+      <nav class="sidebar-nav">
+        <SignedIn>
+          <ul class="sidebar-nav_elements">
+            <SidebarItem
+              v-for="link in navLinks.slice(0, 6)"
+              :key="link.route"
+              :link="link"
+            >
+            </SidebarItem>
+          </ul>
+
+          <ul class="sidebar-nav_elements">
+            <SidebarItem
+              v-for="link in navLinks.slice(6)"
+              :key="link.route"
+              :link="link"
+            >
+            </SidebarItem>
+            <li class="flex-center cursor-pointer gap-2 p-4">
+              <UserButton afterSignOutUrl="/" showName />
+            </li>
+          </ul>
+        </SignedIn>
+
+        <SignedOut>
+          <Button as-child class="button bg-purple-gradient bg-cover">
+            <NuxtLink to="/sign-in">Login</NuxtLink>
+          </Button>
+        </SignedOut>
+      </nav>
+    </div>
+  </aside>
+</template>
---
+++ components/SidebarItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+interface Prop {
+  link: {
+    icon: string;
+    label: string;
+    route: string;
+  };
+}
+const { link } = defineProps<Prop>();
+
+const route = useRoute();
+
+const isActive = computed(() => {
+  return link.route === route.path;
+});
+</script>
+
+<template>
+  <li
+    :key="link.route"
+    :class="`sidebar-nav_element group ${
+      isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+    }`"
+  >
+    <NuxtLink class="sidebar-link" :to="link.route">
+      <NuxtImg
+        :src="link.icon"
+        alt="logo"
+        :width="24"
+        :height="24"
+        :class="{ 'brightness-200': isActive }"
+      />
+      {{ link.label }}
+    </NuxtLink>
+  </li>
+</template>
