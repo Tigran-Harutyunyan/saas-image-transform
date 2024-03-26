dc8d15475cb3d10c054d7884046290446053b3a0
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-26 17:02:43 +0400

    created Header component

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  subtitle?: string;
+}
+
+const { title, subtitle } = defineProps<Props>();
+</script>
+
+<template>
+  <h2 class="h2-bold text-dark-600">{{ title }}</h2>
+  <p v-if="subtitle" class="p-16-regular mt-4">{{ subtitle }}</p>
+</template>
