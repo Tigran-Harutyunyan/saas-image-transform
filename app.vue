3f851366ead8cd4863107886ba746b8e216dcf0c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-29 08:58:09 +0400

    added Toaster to app

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { ClerkLoaded } from "vue-clerk";
+import { Toaster } from "@/components/ui/sonner";
 </script>
 <template>
   <ClerkLoaded>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <Toaster />
   </ClerkLoaded>
 </template>
