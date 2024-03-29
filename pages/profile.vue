14ebda953876708bb2f982c623bd2435c758b26a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-29 15:28:05 +0400

    created Profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,3 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const user = await $fetch("/api/user");
+const { images, page } = useImages(user._id);
+</script>
 
-<template></template>
+<template>
+  <Header title="Profile" />
+
+  <section class="profile">
+    <div class="profile-balance">
+      <p class="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
+      <div class="mt-4 flex items-center gap-4">
+        <NuxtImg
+          src="/assets/icons/coins.svg"
+          alt="coins"
+          :width="50"
+          :height="50"
+          class="size-9 md:size-12"
+        />
+        <h2 class="h2-bold text-dark-600">{{ user.creditBalance }}</h2>
+      </div>
+    </div>
+
+    <div class="profile-image-manipulation">
+      <p class="p-14-medium md:p-16-medium">IMAGE MANIPULATION DONE</p>
+      <div class="mt-4 flex items-center gap-4">
+        <NuxtImg
+          src="/assets/icons/photo.svg"
+          alt="coins"
+          :width="50"
+          :height="50"
+          class="size-9 md:size-12"
+        />
+        <h2 class="h2-bold text-dark-600">{{ images?.data.length }}</h2>
+      </div>
+    </div>
+  </section>
+
+  <section class="mt-8 md:mt-14">
+    <Collection
+      :images="images?.data"
+      :totalPages="images?.totalPages"
+      :page="page"
+    />
+  </section>
+</template>
