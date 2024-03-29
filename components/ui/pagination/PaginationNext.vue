0d401fa173948e0bd45fe007766a3bb7647de6f4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-29 15:32:06 +0400

    added ui components

--- components/ui/pagination/PaginationNext.vue
+++ components/ui/pagination/PaginationNext.vue
@@ -1,29 +1,28 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import { PaginationNext, type PaginationNextProps } from 'radix-vue'
-import { ChevronRight } from 'lucide-vue-next'
-import {
-  Button,
-} from '@/components/ui/button'
-import { cn } from '@/lib/utils'
-
-const props = withDefaults(defineProps<PaginationNextProps & { class?: HTMLAttributes['class'] }>(), {
-  asChild: true,
-})
+import { type HTMLAttributes, computed } from "vue";
+import { PaginationNext, type PaginationNextProps } from "radix-vue";
+import { ChevronRight } from "lucide-vue-next";
+const props = withDefaults(
+  defineProps<PaginationNextProps & { class?: HTMLAttributes["class"] }>(),
+  {
+    asChild: true,
+  }
+);
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
 </script>
 
 <template>
   <PaginationNext v-bind="delegatedProps">
-    <Button :class="cn('w-10 h-10 p-0', props.class)" variant="outline">
+    <div class="flex items-center">
       <slot>
+        <span class="text-sm">Next</span>
         <ChevronRight class="h-4 w-4" />
       </slot>
-    </Button>
+    </div>
   </PaginationNext>
 </template>
