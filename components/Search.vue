dde65d5ab8b397b3034bdd45ec5b6ef8bc45363c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-29 08:53:54 +0400

    created Search component

---
+++ components/Search.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { useDebounceFn } from "@vueuse/core";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+
+const router = useRouter();
+const query = ref();
+
+const debouncedFn = useDebounceFn(() => {
+  const locationSearch = location.search;
+
+  if (query.value) {
+    const newUrl = formUrlQuery({
+      searchParams: locationSearch,
+      key: "query",
+      value: query.value,
+    });
+
+    router.push(newUrl, { scroll: false });
+  } else {
+    const newUrl = removeKeysFromQuery({
+      searchParams: locationSearch,
+      keysToRemove: ["query"],
+    });
+
+    router.push(newUrl, { scroll: false });
+  }
+}, 300);
+
+onMounted(() => {
+  const searchParams = new URLSearchParams(location.search);
+  if (searchParams && searchParams.has("query")) {
+    query.value = searchParams.get("query");
+  }
+});
+</script>
+
+<template>
+  <div class="search">
+    <NuxtImg
+      src="/assets/icons/search.svg"
+      alt="search"
+      :width="24"
+      :height="24"
+    />
+
+    <Input
+      class="search-field"
+      placeholder="Search for tags like art"
+      v-model="query"
+      @update:modelValue="debouncedFn"
+    />
+  </div>
+</template>
