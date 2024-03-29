7b4a97f203c8248f6c2c4739ddb62f01666c0df2
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-29 15:28:59 +0400

    created update page

---
+++ pages/transformations/[imageId]/update/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { transformationTypes } from "@/constants";
+import { type TransformationTypeKey } from "@/types";
+import { type IImage } from "@/lib/database/models/image.model";
+import { toast } from "vue-sonner";
+
+const user = await $fetch("/api/user");
+
+const route = useRoute();
+
+const type = route.params?.type as TransformationTypeKey;
+
+const imageId = route.params?.imageId as string;
+
+const image = await $fetch<IImage>(`/api/image/${imageId}`);
+
+if (!image._id) {
+  toast.info("Image not found");
+  navigateTo("/");
+}
+
+const transformation =
+  transformationTypes[image.transformationType as TransformationTypeKey];
+</script>
+
+<template>
+  <Header :title="transformation?.title" :subtitle="transformation?.subTitle" />
+
+  <section class="mt-10">
+    <ClientOnly>
+      <TransformationForm
+        action="Update"
+        :userId="user?._id"
+        :type="image?.transformationType"
+        :creditBalance="user?.creditBalance"
+        :config="image.config"
+        :data="image"
+      />
+    </ClientOnly>
+  </section>
+</template>
